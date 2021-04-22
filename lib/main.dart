cf78e2be975ac86182265a4084cdb3dc38e16816
Kumar Ballari <kumarballari4@gmail.com>
2021-04-22 12:52:12 +0530

    text widget modified

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'kumar demo app'),
+      home: MyHomePage(title: 'kumar'),
     );
   }
 }
