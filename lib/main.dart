36300ed33e9d911ae92553fa6a071045a7db2e56
Kumar Ballari <kumarballari4@gmail.com>
2021-04-22 17:09:03 +0530

    containers

--- lib/main.dart
+++ lib/main.dart
@@ -28,37 +28,149 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+void alertfunction(BuildContext context) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        scrollable: true,
+        backgroundColor: Colors.amberAccent,
+        title: Text('Contact Us'),
+        content: Text(
+            'mail us at mai@google.com mail us at mai@google.com mail us at mai@google.com mail us at mai@google.com mail us at mai@google.com mail us at mai@google.com mail us at mai@google.com mail us at mai@google.com mail us at mai@google.com mail us at mai@google.com mail us at mai@google.com mail us at mai@google.com mail us at mai@google.com mail us at mai@google.com mail us at mai@google.com mail us at mai@google.com mail us at mai@google.com mail us at mai@google.com mail us at mai@google.com mail us at mai@google.com mail us at mai@google.com mail us at mai@google.commail us at mai@google.com mail us at mai@google.com mail us at mai@google.com mail us at mai@google.com mail us at mai@google.com mail us at mai@google.com mail us at mai@google.com mail us at mai@google.com mail us at mai@google.com mail us at mai@google.com mail us at mai@google.com mail us at mai@google.com mail us at mai@google.com mail us at mai@google.com mail us at mai@google.com mail us at mai@google.com mail us at mai@google.com mail us at mai@google.com mail us at mai@google.com mail us at mai@google.com mail us at mai@google.com mail us at mai@google.commail us at mai@google.com mail us at mai@google.com mail us at mai@google.com mail us at mai@google.com mail us at mai@google.com mail us at mai@google.com mail us at mai@google.com mail us at mai@google.com mail us at mai@google.com mail us at mai@google.com mail us at mai@google.com mail us at mai@google.com mail us at mai@google.com mail us at mai@google.com mail us at mai@google.com mail us at mai@google.com mail us at mai@google.com mail us at mai@google.com mail us at mai@google.com mail us at mai@google.com mail us at mai@google.com mail us at mai@google.com'),
+        actions: <Widget>[
+          FlatButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text('Close'),
+          ),
+        ],
+      );
+    },
+  );
+}
+
 class _MyHomePageState extends State<MyHomePage> {
   int count = 0, count2 = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(40.0),
-          child: Column(children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextButton(
-                  onPressed: () {},
-                  child: Text(
-                    'text button',
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 50.0,
-            ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [],
-            ),
-          ]),
-        ),
+      appBar: AppBar(
+        title: Text(widget.title),
+        elevation: 6.0,
       ),
+      body: Center(
+          child: ListView(
+        padding: EdgeInsets.all(10.0),
+        children: <Widget>[
+          TextButton(
+              onPressed: () {
+                alertfunction(context);
+              },
+              child: Text('Contact us')),
+          Container(
+            padding: EdgeInsets.all(10.0),
+            height: 200.0,
+            width: MediaQuery.of(context).size.width,
+            child: Text(
+              'Container 1',
+              style: GoogleFonts.roboto(
+                fontSize: 20.0,
+              ),
+            ),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20.0),
+              color: Colors.amberAccent,
+              border: Border.all(
+                width: 1.0,
+                color: Colors.black,
+                style: BorderStyle.solid,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          Container(
+            padding: EdgeInsets.all(10.0),
+            height: 200.0,
+            width: MediaQuery.of(context).size.width - 20,
+            child: Text(
+              'Container 2',
+              style: GoogleFonts.roboto(
+                fontSize: 20.0,
+              ),
+            ),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20.0),
+              color: Colors.blueAccent,
+              border: Border.all(
+                  width: 1.0, color: Colors.black, style: BorderStyle.solid),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Container(
+              padding: EdgeInsets.all(10.0),
+              height: 200.0,
+              width: MediaQuery.of(context).size.width,
+              child: Text(
+                'Container 6',
+                style: GoogleFonts.roboto(
+                  fontSize: 20.0,
+                ),
+              ),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20.0),
+                color: Colors.pinkAccent,
+                border: Border.all(
+                    width: 1.0, color: Colors.black, style: BorderStyle.solid),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Container(
+              padding: EdgeInsets.all(10.0),
+              height: 200.0,
+              width: MediaQuery.of(context).size.width,
+              child: Text(
+                'Container 3',
+                style: GoogleFonts.roboto(
+                  fontSize: 20.0,
+                ),
+              ),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20.0),
+                color: Colors.greenAccent,
+                border: Border.all(
+                    width: 1.0, color: Colors.black, style: BorderStyle.solid),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Container(
+              padding: EdgeInsets.all(10.0),
+              height: 200.0,
+              width: MediaQuery.of(context).size.width,
+              child: Text(
+                'Container 4',
+                style: GoogleFonts.roboto(
+                  fontSize: 20.0,
+                ),
+              ),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20.0),
+                color: Colors.purpleAccent,
+                border: Border.all(
+                    width: 1.0, color: Colors.black, style: BorderStyle.solid),
+              ),
+            ),
+          )
+        ],
+      )),
     );
   }
 }
